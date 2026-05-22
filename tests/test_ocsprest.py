c9ab4a23d402d377f47794a78ba557cfd4555e33
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-05-22 23:34:57 +0300

    chore: bump version to 1.0.7+260427

--- tests/test_ocsprest.py
+++ tests/test_ocsprest.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.0.7+260427"
+    assert __version__ == "1.0.8+260522"
 
 
 def test_healthcheck(client: TestClient) -> None:
